2268199ba8ed61acb011660a7ce84c4db94277c2
migbro <miguel.a.brown@gmail.com>
2023-03-14 14:40:41 -0400

    :wrench: fix bug introduced by suggested commit :trophy: address PR concerns and suggestions

--- analyses/collapse-rnaseq/cwl/scripts/liftover_collapse_rnaseq.py
+++ analyses/collapse-rnaseq/cwl/scripts/liftover_collapse_rnaseq.py
@@ -9,7 +9,7 @@ import sys
 from statistics import mean
 
 
-def update_gtf_dict(in_dict, in_text):
+def update_gtf_dict(gtf_dict, in_text, tx_type, tx_desc, id_find, name_find):
     """
     Update ensg - sym ID dictionary on gene entries only
     """
@@ -18,72 +18,62 @@ def update_gtf_dict(in_dict, in_text):
         try:
             gene_id = id_find.match(data[tx_desc]).group(1)
             gene_name = name_find.match(data[tx_desc]).group(1)
-            in_dict[gene_id] = gene_name
+            gtf_dict[gene_id] = gene_name
         except Exception as e:
-            sys.stderr.write(str(e) + '\n')
-            sys.stderr.write("Failed to parse gene ID and name from " + in_text)
+            print(str(e), file=sys.stderr)
+            print("Failed to parse gene ID and name from " + in_text, file=sys.stderr)
 
 
-def liftover(in_data, out_lift, n, l):
+def liftover(line_from_infile, gene_sym_ct_dict, g_idx, n_idx, gtf_dict, d_idx):
     """
     Update gene symbol if found in dict, leave alone if not
     """
-    if l % n == 0:
-        sys.stderr.write("Processed " + str(l) + " lines\n")
-        sys.stderr.flush()
-    data = in_data.rstrip('\n').split('\t')
+    data = line_from_infile.rstrip('\n').split('\t')
     cur_id = data[g_idx].split('.')[0]
     if cur_id in gtf_dict:
         data[n_idx] = gtf_dict[cur_id]
     gene_sym = data[n_idx]
-    if gene_sym not in sym_ct:
-        sym_ct[gene_sym] = 1
+    if gene_sym not in gene_sym_ct_dict:
+        gene_sym_ct_dict[gene_sym] = 1
     else:
-        sym_ct[gene_sym] += 1
-    out_data = "{}\t{}\t{}\n".format(cur_id, gene_sym, '\t'.join(data[d_idx:])
-    out_lift.write(out_data)
-    l += 1
-    return l
+        gene_sym_ct_dict[gene_sym] += 1
+    out_data = "{}\t{}\t{}\n".format(cur_id, gene_sym, '\t'.join(data[d_idx:]))
+    return out_data
 
 
-def collate_dups(in_data, in_dict, ct, out_file, l):
+def collate_dups(in_data, dup_gene_sym_dict, ct, gene_sym_ct_dict, d_idx, n_idx):
     """
-    Checks if entry is in dup dict (really smybol seen > 1 in sym_ct),
+    Checks if entry is in dup dict (really symbol seen > 1 in gene_sym_ct_dict),
     then add to dict ot process, or output  to open file handle as-is if not repeated
     """
-    if l % n == 0:
-            sys.stderr.write("Processed " + str(l) + " lines\n")
-            sys.stderr.flush()
     data = in_data.rstrip('\n').split('\t')
     gene_sym = data[n_idx]
-    if sym_ct[gene_sym] > 1:
-        if gene_sym not in dup_dict:
-            dup_dict[gene_sym] = []
+    out_data = None
+    if gene_sym_ct_dict[gene_sym] > 1:
+        if gene_sym not in dup_gene_sym_dict:
+            dup_gene_sym_dict[gene_sym] = []
             ct += 1
         # cast as int for later
-        dup_dict[gene_sym].append(list(map(float, data[d_idx:])))
+        dup_gene_sym_dict[gene_sym].append(list(map(float, data[d_idx:])))
     else:
-        out_data = gene_sym + "\t" + "\t".join(data[d_idx:]) + '\n'
-        out_file.write(out_data)
-    l += 1
-    return ct, l
+        out_data = "{}\t{}\n".format(gene_sym, '\t'.join(data[d_idx:]))
+    return ct, out_data
 
 
-def collapse_dups(dup_dict, out_file):
+def collapse_dups(dup_gene_sym_dict):
     """
     Get highest mean for repeated gene symbols and output that row
     """
-    for gene_sym in dup_dict:
+    for gene_sym in dup_gene_sym_dict:
         means = []
-        for data in dup_dict[gene_sym]:
+        for data in dup_gene_sym_dict[gene_sym]:
             means.append(mean(data))
         top_idx = means.index(max(means))
-        out_data = gene_sym + "\t" + "\t".join(list(map(str, dup_dict[gene_sym][top_idx]))) + '\n'
-        out_file.write(out_data)
+        out_data = "{}\t{}\n".format(gene_sym, '\t'.join(list(map(str, dup_gene_sym_dict[gene_sym][top_idx]))))
+        return out_data
 
 
-if __name__ == '__main__':
-    
+def main():
     parser = argparse.ArgumentParser(
         description="Script to use a GTF and expression matrix with ENSG to liftover gene symbols."
     )
@@ -122,16 +112,16 @@ if __name__ == '__main__':
     # set up pattern matcher ahead of time for efficiency
     id_find = re.compile(r"gene_id \"(ENSG\d+)\.\d+\";")
     name_find = re.compile(r".*gene_name \"(\S+)\";")
-    sys.stderr.write('Indexing gtf\n')
+    print('Indexing gtf', file=sys.stderr)
     with (gzip.open if args.gtf.endswith("gz") else open)(args.gtf, "rt", encoding="utf-8") as gtf:
         for line in gtf:
             if not line.startswith('##'):
-                update_gtf_dict(gtf_dict, line)
+                update_gtf_dict(gtf_dict, line, tx_type, tx_desc, id_find, name_find)
     # track sym occurrences if collapse flag given for later processing
-    sym_ct = {}
+    gene_sym_ct_dict = {}
     with (gzip.open if args.table.endswith("gz") else open)(args.table, "rt", encoding="utf-8") as table:
         if args.skip:
-            sys.stderr.write('Skipping ' + str(args.skip) + ' lines\n')
+            print('Skipping ' + str(args.skip) + ' lines', file=sys.stderr)
             for i in range(args.skip):
                 skip = next(table)
         head = next(table)
@@ -141,38 +131,47 @@ if __name__ == '__main__':
         n_idx = header.index(args.gene_name)
         # make assumption that data will start after ID and name
         d_idx = max(g_idx, n_idx) + 1
-        sys.stderr.write('Processing table\n')
-        sys.stderr.flush()
+        print('Processing table', file=sys.stderr)
         out_lift_fn = args.out + '.liftover.tsv'
         out_lift = open(out_lift_fn, 'w')
         # Some counters to track progress
         n = 10000
         l = 0
-        out_head = ('gene_id\tgene_name\t' + '\t'.join(header[d_idx:]))
+        out_head = "{}\t{}\t{}\n".format('gene_id', 'gene_name', '\t'.join(header[d_idx:]))
         out_lift.write(out_head)
         for line in table:
-            l = liftover(line, out_lift, n, l)
+            if l % n == 0:
+                print("Processed " + str(l) + " lines", file=sys.stderr)
+            out_data = liftover(line, gene_sym_ct_dict, g_idx, n_idx, gtf_dict, d_idx)
+            out_lift.write(out_data)
+            l += 1
         # dump out rest of leftover lines
         out_lift.close()
     
     if args.collapse:
-        sys.stderr.write("Collapse flag given. Collating dups from liftover\n")
-        sys.stderr.flush()
-        dup_dict = {}
+        print("Collapse flag given. Collating dups from liftover", file=sys.stderr)
+        dup_gene_sym_dict = {}
         dup_ct = 0
         out_collapse_fn = args.out + '.collapsed.tsv'
         out_collapse = open(out_collapse_fn, 'w')
         with (gzip.open if out_lift_fn.endswith("gz") else open)(out_lift_fn, "rt", encoding="utf-8") as lifted:
             # already got header as array, so can skip splitting
             skip = next(lifted)
-            out_head = 'gene_name' + '\t' + '\t'.join(header[d_idx:]) + '\n'
+            out_head = "{}\t{}\n".format('gene_name', '\t'.join(header[d_idx:]))
             out_collapse.write(out_head)
             l = 0
             for line in lifted:
-                dup_ct, l = collate_dups(line, dup_dict, dup_ct, out_collapse, l)
-        sys.stderr.write("Processing " + str(dup_ct) + " repeat gene symbols and choosing highest mean expression\n")
-        sys.stderr.flush()
-        collapse_dups(dup_dict, out_collapse)
-        sys.stderr.write("Done!\n")
-        sys.stderr.flush()
+                dup_ct, out_data = collate_dups(line, dup_gene_sym_dict, dup_ct, gene_sym_ct_dict, d_idx, n_idx)
+                if l % n == 0:
+                    print("Processed " + str(l) + " lines", file=sys.stderr)
+                if out_data:
+                    out_collapse.write(out_data)
+                l += 1
+        print("Processing " + str(dup_ct) + " repeat gene symbols and choosing highest mean expression", file=sys.stderr)
+        out_data = collapse_dups(dup_gene_sym_dict)
+        out_collapse.write(out_data)
+        print("Done!", file=sys.stderr)
 
+
+if __name__ == '__main__':
+    main()
