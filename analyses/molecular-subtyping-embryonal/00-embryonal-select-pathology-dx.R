3cfd353e84cb2e8750c994064985c9ade7c215b1
jharenza <jharenza@gmail.com>
2023-04-25 20:43:54 +0000

    one more mixed path dx update in JSON

--- analyses/molecular-subtyping-embryonal/00-embryonal-select-pathology-dx.R
+++ analyses/molecular-subtyping-embryonal/00-embryonal-select-pathology-dx.R
@@ -19,6 +19,7 @@ output_file <- file.path(root_dir,
 path_dx_terms<- c(
   "Supratentorial or Spinal Cord PNET",
   "Embryonal Tumor with Multilayered Rosettes (ETMR)",
+  "Embryonal Tumor with Multilayered Rosettes (ETMR);High-grade glioma/astrocytoma (WHO grade III/IV)",
   "Embryonal Tumor NOS"
 )
 
