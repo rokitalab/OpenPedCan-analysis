7979cad71385d66738a7babef993ae68832edbb3
zzgeng <zz.geng@hotmail.com>
2023-06-27 03:37:46 +0000

    update run_consensus_call.sh

--- analyses/copy_number_consensus_call_manta/run_consensus_call.sh
+++ analyses/copy_number_consensus_call_manta/run_consensus_call.sh
@@ -27,7 +27,7 @@ python3 scripts/merged_to_individual_files.py \
     --manta ../../data/sv-manta.tsv.gz \
     --cnvkit ../../data/cnv-cnvkit.seg.gz \
     --freec ../../data/cnv-controlfreec.tsv.gz \
-    --histologies ../missing_wgs \
+    --histologies ../missing_wgs.tsv \
     --snake $SCRATCHDIR/config_snakemake.yaml \
     --scratch $SCRATCHDIR \
     --uncalled results/uncalled_samples.tsv
