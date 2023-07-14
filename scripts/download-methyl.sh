4a84a75397c6dbb67354be6f31ae408916cefec1
Jo Lynne Rokita <jharenza@gmail.com>
2023-07-14 13:56:20 -0400

    Updating the code to pull the md5sum first, put the files into the dir if not already exists

--- scripts/download-methyl.sh
+++ scripts/download-methyl.sh
@@ -3,21 +3,25 @@
 set -e
 set -o pipefail
 
-# download methylation files from the OpenPedCan data v12 data release s3 bucket
+# download methylation files from the OpenPedCan data v13 data release s3 bucket
 
 URL="https://d3b-openaccess-us-east-1-prd-pbta.s3.amazonaws.com/open-targets"
 RELEASE="v13"
 
 # check if the release folder exists, if not, create a release folder
-[ ! -d "../data/$RELEASE/" ] && mkdir ../data/$RELEASE/
+#[ ! -d "../data/$RELEASE/" ] && mkdir ../data/$RELEASE/
+
+# The md5sum file provides our single point of truth for which files are in a release.
+curl --create-dirs $URL/$RELEASE/methyl-md5sum.txt -o ../data/$RELEASE/methyl-md5sum.txt -z ../data/$RELEASE/methyl-md5sum.txt
 
 FILES=(`tr -s ' ' < ../data/$RELEASE/methyl-md5sum.txt | cut -d ' ' -f 2` release-notes.md)
+
 for file in "${FILES[@]}"
 do
   if [ ! -e "../data/$RELEASE/$file" ]
   then
     echo "Downloading $file"
-    curl $URL/$RELEASE/$file -o ../data/$RELEASE/$file
+    curl --create-dirs $URL/$RELEASE/$file -o ../data/$RELEASE/$file
   fi
 done
 
