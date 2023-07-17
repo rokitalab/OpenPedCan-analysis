aea66851ad03361f9cca2dc23735439bd5e7ca91
zzgeng <zz.geng@hotmail.com>
2023-07-17 19:50:16 +0000

    update script for merging cnv files

--- analyses/copy_number_consensus_call/merge_cnv_file.R
+++ analyses/copy_number_consensus_call/merge_cnv_file.R
@@ -4,11 +4,11 @@ library(tidyverse)
 root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
 cnv_gatk_dir <- file.path(root_dir, "analyses", "copy_number_consensus_call", "results")
 cnv_manta_dir <- file.path(root_dir, "analyses", "copy_number_consensus_call_manta", "results")
-output_file <- file.path(cnv_gatk_dir,  "cnv-consensus_full.seg.gz")
+output_file <- file.path(cnv_gatk_dir,  "cnv-consensus.seg.gz")
 
 # read cnv files
-cnv_gatk <- readr::read_tsv(file.path(cnv_gatk_dir, "cnv-consensus.seg.gz"))
-cnv_manta <- readr::read_tsv(file.path(cnv_manta_dir, "cnv-consensus.seg.gz"))
+cnv_gatk <- readr::read_tsv(file.path(cnv_gatk_dir, "cnv-consensus-freec-ckit-gatk.seg.gz"))
+cnv_manta <- readr::read_tsv(file.path(cnv_manta_dir, "cnv-consensus-freec-ckit-manta.seg.gz"))
 
 # merge two files together
 merged <- rbind(cnv_gatk, cnv_manta) %>% 
