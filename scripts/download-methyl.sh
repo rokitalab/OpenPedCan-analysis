0ce5bbbae60c20e038680eb50de3ad8e20705f3d
zzgeng <zz.geng@hotmail.com>
2023-07-17 14:52:48 +0000

    set base directories based on comment

--- scripts/download-methyl.sh
+++ scripts/download-methyl.sh
@@ -8,28 +8,41 @@ set -o pipefail
 URL="https://d3b-openaccess-us-east-1-prd-pbta.s3.amazonaws.com/open-targets"
 RELEASE="v13"
 
+# Set the working directory to the directory of this file
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# If RUN_LOCAL is used, the time-intensive steps are skipped because they cannot
+# be run on a local computer -- the idea is that setting RUN_LOCAL=1 will allow for
+# local testing running/testing of all other steps
+RUN_LOCAL=${RUN_LOCAL:-0}
+
+# Get base directory of project
+cd ..
+BASEDIR="$(pwd)"
+cd -
+
 # check if the release folder exists, if not, create a release folder
-[ ! -d "../data/$RELEASE/" ] && mkdir ../data/$RELEASE/
+[ ! -d "$BASEDIR/data/$RELEASE/" ] && mkdir $BASEDIR/data/$RELEASE/
 
 # The md5sum file provides our single point of truth for which files are in a release.
-curl --create-dirs $URL/$RELEASE/methyl-md5sum.txt -o ../data/$RELEASE/methyl-md5sum.txt -z ../data/$RELEASE/methyl-md5sum.txt
+curl --create-dirs $URL/$RELEASE/methyl-md5sum.txt -o $BASEDIR/data/$RELEASE/methyl-md5sum.txt -z $BASEDIR/data/$RELEASE/methyl-md5sum.txt
 
-FILES=(`tr -s ' ' < ../data/$RELEASE/methyl-md5sum.txt | cut -d ' ' -f 2` release-notes.md)
+FILES=(`tr -s ' ' < $BASEDIR/data/$RELEASE/methyl-md5sum.txt | cut -d ' ' -f 2` release-notes.md)
 
 for file in "${FILES[@]}"
 do
-  if [ ! -e "../data/$RELEASE/$file" ]
+  if [ ! -e "$BASEDIR/data/$RELEASE/$file" ]
   then
     echo "Downloading $file"
-    curl --create-dirs $URL/$RELEASE/$file -o ../data/$RELEASE/$file
+    curl --create-dirs $URL/$RELEASE/$file -o $BASEDIR/data/$RELEASE/$file
   fi
 done
 
 #check md5sum
-cd ../data/$RELEASE
+cd $BASEDIR/data/$RELEASE
 echo "Checking MD5 hashes..."
 md5sum -c methyl-md5sum.txt
-cd ../../
+cd $BASEDIR
 
 # Make symlinks in data/ to the files in the just downloaded release folder.
 for file in "${FILES[@]}"
