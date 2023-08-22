4490e87c163514a6fcea851238ab92d75fc2158e
antoniachroni <chronia@chop.edu>
2023-08-21 21:47:10 -0400

    Replace input_dir with data_dir

--- analyses/tmb-calculation/run_tmb_calculation.sh
+++ analyses/tmb-calculation/run_tmb_calculation.sh
@@ -14,7 +14,6 @@ cd "$script_directory" || exit
 # Set up paths to data files consumed by analysis
 data_dir="../../data"
 scratch_dir="../../scratch"
-input_dir="input"
 
 # BED and GTF file paths
 cds_file="${scratch_dir}/gencode.v39.primary_assembly.annotation.bed"
@@ -24,7 +23,7 @@ wgs_bed="${scratch_dir}/intersect_strelka_mutect2_vardict_WGS.bed"
 variant_file="${data_dir}/snv-consensus-plus-hotspots.maf.tsv.gz"
 
 # sample to BED mapping file
-mapping_file="${input_dir}/biospecimen_id_to_bed_map.tsv"
+mapping_file="${data_dir}/biospecimen_id_to_bed_map.tsv"
 
 # Histology file
 histology_file="${data_dir}/histologies-base.tsv"
