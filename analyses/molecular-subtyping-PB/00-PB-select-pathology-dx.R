6ffaa0e5379be3d993a0449a0c50bc2cdf5f48fa
Zhuangzhuang Geng <zz.geng@hotmail.com>
2023-12-06 17:40:43 -0500

    add Pineoblastoma subtype module

---
+++ analyses/molecular-subtyping-PB/00-PB-select-pathology-dx.R
@@ -0,0 +1,48 @@
+# In this script we will be gathering pathology diagnosis
+# and pathology free text diagnosis terms to select pineoblastoma (PB)
+# samples for following PB subtyping analysis and save 
+# the json file in PB-subset folder
+
+library(tidyverse)
+
+## Directories
+
+root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
+
+output_dir <- file.path(root_dir,
+                        "analyses",
+                        "molecular-subtyping-PB",
+                        "PB-subset")
+if(!dir.exists(output_dir)){
+  dir.create(output_dir)
+}
+
+output_file <- file.path(output_dir, "PB_subtyping_path_dx_strings.json")                         
+
+## Read histologies file
+histo <- readr::read_tsv(file.path(root_dir, "data", "histologies-base.tsv")) 
+
+## the path dx and path free text is based on the methylation subypes:  
+##  "PB_FOXR2", "PB_GRP1A", "PB_GRP1B", "PB_GRP2", "PIN_RB"
+## The `pathology_diagnosis` fields for PB
+exact_path_dx <- c("Pineoblastoma", "Supratentorial or Spinal Cord PNET")
+
+## The `pathology_diagnosis` fields for PB
+path_free_text_exact <- c("pineoblastoma, who grade iv",                     
+                         "pineoblastoma who iv",                            
+                         "pineoblastoma",                                   
+                         "embryonal tumor in keeping with pineoblastoma",   
+                         "pineoblastoma (who grade iv)",                    
+                         "pineoblastoma with dicer1 mutation, who grade iv",
+                         "pineoblastoma who grade iv",                      
+                         "pnet - pineoblastoma with calcification",         
+                         "pnet",                                            
+                         "pineal anlage tumor",                             
+                         "pineoblastoma, who grade 4")
+
+## Create a list with the strings we'll use for inclusion
+term_list <- list(exact_path_dx = exact_path_dx,
+                  path_free_text_exact = path_free_text_exact)
+
+# Save the list as json
+writeLines(jsonlite::prettify(jsonlite::toJSON(term_list)), output_file)
