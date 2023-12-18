79f68eae5ebd5af0026473cbc2ad8b4312366d46
Zhuangzhuang Geng <zz.geng@hotmail.com>
2023-12-18 17:25:15 -0500

    change the format of tumor only

--- analyses/copy_number_consensus_call_manta/scripts/merge_cnv_tumor_only.R
+++ analyses/copy_number_consensus_call_manta/scripts/merge_cnv_tumor_only.R
@@ -4,7 +4,7 @@ root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
 data_dir <- file.path(root_dir, "data")
 
 cnv_controlfreec <- read_tsv(file.path(data_dir, "cnv-controlfreec.tsv.gz"))
-cnv_tumor_only <- read_tsv(file.path(data_dir, "cnv-controlfreec-tumor-only.tsv"))
+cnv_tumor_only <- read_tsv(file.path(data_dir, "cnv-controlfreec-tumor-only.tsv.gz"))
 
 cnv_merged <- cnv_tumor_only %>% 
   bind_rows(cnv_controlfreec) %>% 
