ac8c77898fc6eabcbc5f713ea92dd13d54ccba56
jharenza <jharenza@gmail.com>
2024-01-01 21:19:14 +0000

    test changing perm of rds file

--- analyses/molecular-subtyping-chordoma/run-molecular-subtyping-chordoma.sh
+++ analyses/molecular-subtyping-chordoma/run-molecular-subtyping-chordoma.sh
@@ -19,12 +19,18 @@ script_directory="$(perl -e 'use File::Basename;
   use Cwd "abs_path";
   print dirname(abs_path(@ARGV[0]));' -- "$0")"
 cd "$script_directory" || exit
+scratch_dir="../../scratch"
+
 
 Rscript 00-chordoma-select-pathology-dx.R
 
 if [ "$SUBSET" -gt "0" ]; then
   Rscript --vanilla 00-subset-files-for-chordoma.R
 fi
+
+# change permissions of the rds file
+scratch_dir="../../scratch"
+chmod 644 "${scratch_dir}/chordoma-only-gene-expression-rsem-tpm-collapsed.rds"
 
 Rscript -e "rmarkdown::render('01-Subtype-chordoma.Rmd', clean = TRUE)"
 
